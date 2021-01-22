52d5e5b7527a5dc718e393310d3da22028139601
Daniela Gordas <daniela.gordas@gmail.com>
2021-01-22 16:19:39 +0100

    changed gallery component name from capital letter to small letter

--- src/pages/landscape.js
+++ src/pages/landscape.js
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from 'gatsby'
 import '../styles/styles.scss'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Gallery from '../components/Gallery'
+import Gallery from '../components/gallery'
 
 const useGallery = () => {
   const data = useStaticQuery(graphql`
