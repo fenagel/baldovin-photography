8fba983d6e76476c35a3e720b334192cb4459012
Felix Nagel <felixnagel105@gmail.com>
2021-01-22 15:12:43 +0100

    refactored grid layout for gallery pages

--- src/pages/nature.js
+++ src/pages/nature.js
@@ -1,16 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { useStaticQuery, graphql } from 'gatsby'
+import '../styles/styles.scss'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Gallery from '../components/Gallery'
+
+const useGallery = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(
+        filter: { sourceInstanceName: { eq: "nature" } }
+      ) {
+        nodes {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return data.allFile.nodes.map(node => ({
+    ...node.childImageSharp, // Note that we're spreading the childImageSharp object here
+    id: node.id,
+  }));
+};
 
 const NaturePage = () => (
   <Layout>
     <SEO title="Nature" />
-    <h1>Nature</h1>
-    <p>Welcome to nature images</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1 className="page-header">
+      Nature
+    </h1>
+    <Gallery usegallery={useGallery()} />
   </Layout>
 )
+
 
 export default NaturePage;
